c77209fd0ea4a640e33955443ed6dbb14fd6a523
Orr Goren <orrgorenn@gmail.com>
2020-11-17 17:17:11 +0200

    v0.1

--- server/index.js
+++ server/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const socketio = require('socket.io');
 const http = require('http');
 
+const { addUser, removeUser, getUser, getUsersInRoom } = require('./users.js');
+
 const PORT = process.env.PORT || 2058;
 
 const router = require('./router');
@@ -13,8 +15,38 @@ const io = socketio(server);
 io.on('connection', (socket) => {
     console.log('New connection.');
 
+    socket.on('join', ({name, room}, callback) => {
+        const { error, user } = addUser({ id: socket.id, name, room });
+
+        if(error) return callback(error);
+
+        socket.emit('message', { user: 'מערכת', text: `ברוכים הבאים, ${user.name}!` });
+        socket.broadcast.to(user.room).emit('message', { user: 'מערכת', text: `גם ${user.name} פה!` })
+
+        socket.join(user.room);
+
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+
+        callback();
+    });
+
+    socket.on('sendMessage', (message, callback) => {
+        const user = getUser(socket.id);
+
+        io.to(user.room).emit('message', { user: user.name, text: message });
+        io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room) });
+
+        callback();
+    });
+
+
     socket.on('disconnect', () => {
-        console.log('Closed connection.');
+        const user = removeUser(socket.id);
+
+        if(user) {
+            io.to(user.room).emit('message', { user: 'מערכת', text: `${user.name} עזב את החדר.` });
+            io.to(user.room).emit('roomData', { room: user.room, users: getUsersInRoom(user.room)});
+        }
     });
 });
 
